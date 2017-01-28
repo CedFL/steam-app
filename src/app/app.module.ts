0472f5ddc108c8cb61af1f922bbcfc56c02ec44e
ErwanFily <erwanfily@hotmail.fr>
2017-01-28 22:17:40 +0100

    Revert "Prout"
    This reverts commit 0723f1c3160f2063b38c38fe0fdb54b0f7782a9c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { ROUTES } from './app.routes';
 
 import { GameService } from './game.service';
 
@@ -11,21 +9,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BiblioComponent } from './biblio/biblio.component';
-import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BiblioComponent,
-    PageComponent
+    BiblioComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot(ROUTES)
+    HttpModule
   ],
   providers: [GameService],
   bootstrap: [AppComponent]
