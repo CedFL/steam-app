323aeb60fc505a4b6bf2f0f0411d10a9f7829f0b
CedFL <cedric.fernandezlebrot@devinci.fr>
2017-01-28 22:12:30 +0100

    refonte accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BiblioComponent } from './biblio/biblio.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BiblioComponent
+    BiblioComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
